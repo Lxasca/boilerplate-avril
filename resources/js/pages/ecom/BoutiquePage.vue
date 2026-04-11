f4c77047f7fc210f2eaab6f4b83ef8a28c71fee8
Lxasca <aestdata@gmail.com>
2026-04-11 21:32:06 +0700

    Affichage des instances de Product dans BoutiquePage

--- resources/js/pages/ecom/BoutiquePage.vue
+++ resources/js/pages/ecom/BoutiquePage.vue
@@ -1,9 +1,37 @@
 <template>
     <h1>Boutique</h1>
+
+    <section>
+        <div v-for="product in products" :key="product.id">
+            {{ product.name }}
+        </div>
+    </section>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'BoutiquePage'
+    name: 'BoutiquePage',
+    data() {
+        return {
+            products: []
+        }
+    },
+    mounted() {
+        this.getProducts();
+    },
+    methods: {
+        getProducts() {
+            axios
+            .get('/products')
+            .then((response) => {
+                this.products = response.data;
+            })
+            .catch((error) => {
+                console.log('erreur ', error)
+            })
+        }
+    }
 }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Ecom\ProductController;
 use Illuminate\Support\Facades\Route;
+
+// ecom
+Route::get('/products', [ProductController::class, 'index']);
 
 Route::get('/{any}', function () {
     return view('welcome');
