02355be7035c5c7108b6f9b7329d954b5ed2f9be
Lxasca <aestdata@gmail.com>
2026-04-13 13:32:09 +0700

    Configuration des Models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\OrderItem;
+use App\Models\PromoCode;
+use App\Models\User;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['user_id', 'promo_code_id', 'status', 'total_ht', 'total_ttc'];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function promoCode()
+    {
+        return $this->belongsTo(PromoCode::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
