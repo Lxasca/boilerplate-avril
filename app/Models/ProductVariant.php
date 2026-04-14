b4240a42150925d4814d8c8749136e0710a1232b
Lxasca <aestdata@gmail.com>
2026-04-14 10:59:27 +0700

    Création du Model ProductVariant et de sa migration

---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariant extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductVariantFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'capacity', 'size', 'color', 'product_id'
+    ];
+}
